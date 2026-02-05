f40e1e7245ad8d766df0625514850b11701c8c42
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-02-05 21:20:20 +0530

    doing experiment

---
+++ Classwork/flagsAndModule/files.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
+
+fs.readFile("data.txt", {flag:"r" }, (err, data)=>{
+  if(err){
+    console.log("Error reading file:", err);
+  } else {
+    console.log("File contents:",data);
+  }
+});
+
+
+//hello just for commit i am very tired and sleepy sry sir i will do better next time i promise <3
