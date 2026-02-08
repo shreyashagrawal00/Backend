3d9294094f09eefe19aaadb70cdb7c0ae450285d
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-02-08 13:54:49 +0530

    Add initial calculation.js file with commented-out functions

---
+++ Classwork/understandig_nodejs/calculation.js
@@ -0,0 +1,18 @@
+
+// const sum = (a,b)=>{
+//     return a+b;
+// } 
+// const sub = (a,b)=>{
+//     return a-b;
+// } 
+
+// module.exports=sum;
+//module.exports=sub; last wla consider krta h agr jyda module hote h
+
+// const sum = (a,b)=>{
+//     return a+b;
+// } 
+// const sub = (a,b)=>{
+//     return a-b;
+// } 
+// fs.open("file.txt","r+", callback);
