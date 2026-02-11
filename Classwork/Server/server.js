51b7f634a895ae8debbc6f880736f95fe3ea61ca
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-02-11 12:08:08 +0530

    making server

---
+++ Classwork/Server/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });   
+    res.end('Hello, World!');
+  }
+});
+
+const PORT = 3000;  
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
